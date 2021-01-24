a3b543c21d54ff149edf589c4dba4a23af3787ec
sanotaku <saturday0902@hotmail.co.jp>
2021-01-24 20:28:18 +0900

    ・スローストキャスティクスの最適化モジュールを追加。 ・モジュールを整理。

---
+++ optimizer_module/optimizer_rsi.py
@@ -0,0 +1,156 @@
+# import libraries
+import sys
+from typing import List
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import talib as ta
+
+
+class RsiOptimizer(object):
+    """
+    Note:
+    RSI売買を仮想で行い、パラメータをグリッドサーチで最適化
+    パラメータ
+        :span       RSIのspan
+        :buy_thres  購入する閾値
+        :sell_thres 売る閾値
+    """
+    def __init__(self):
+        self._buy_thres_low = None
+        self._buy_thres_high = None
+        self._sell_thres_low = None
+        self._sell_thres_high = None
+        self._span_low = None
+        self._span_high = None
+
+        self.df = None
+
+        self._best_parms = {
+            "span": 0,
+            "buy_thres": 0,
+            "sell_thres": 0,
+            "profit": 0
+        }
+
+    def set_params(self,
+                   buy_thres: List[int],
+                   sell_thres: List[int],
+                   spans: List[int]):
+        """
+        set param
+        Args:
+            buy_thres(List)  : RSI buy limit[low, high]
+            sell_thres(List) : RSI sell limit[low, high]
+            span_thres(List) : RSI span range[low, high]
+        """
+        self._buy_thres_low = buy_thres[0]
+        self._buy_thres_high = buy_thres[1]
+        self._sell_thres_low = sell_thres[0]
+        self._sell_thres_high = sell_thres[1]
+        self._span_low = spans[0]
+        self._span_high = spans[1]
+
+    def _calculate_rsi_profit(self, df, span: int, low_thres: int, high_thres: int) -> float:
+        """
+        inner function
+        RSIで売買し、その結果利損益を返す
+        Args:
+            df(pandas Dataframe)   : stock data
+            span(int)              : RSI span
+        Return:
+            pl_amount_retio(float) : profit and loss from RSI
+        """
+        # set RSI cal data
+        df["RSI"] = ta.RSI(df["Close"], span)
+
+        # Args
+        stock = 0
+        start_asset = 10000
+        asset = start_asset
+
+        for i in range(len(df)):
+
+            # buy
+            if df["RSI"][i] > low_thres and df["RSI"][i - 1] < low_thres:
+                if asset != 0:
+                    stock = asset / df["Close"][i]
+                    asset = 0
+                # print(f"buy : {round(df['RSI'][i], 1)} -> {round(df['RSI'][i-1], 1): 5} | stock: {round(stock, 2)}")
+
+            # sell
+            if df["RSI"][i] < high_thres and df["RSI"][i - 1] > high_thres:
+                if stock != 0:
+                    asset = stock * df["Close"][i]
+                    stock = 0
+                # print(f"sell: {round(df['RSI'][i], 1)} -> {round(df['RSI'][i-1], 1): 5} | asset: {round(asset, 2)}")
+
+        # If you still have stock, sell it
+        last_asset = asset + stock * df["Close"][-1]
+        pl_amount_retio = last_asset / start_asset * 100
+        # print(f"PL Amount: {round(pl_amount_retio, 2)}")
+        return pl_amount_retio
+
+    def run(self, df):
+        """
+        Note: 解析開始コマンド
+        Args:
+            df(pandas dataframe)     : stock data
+        Return:
+            result(pandas Dataframe) : 解析結果
+        """
+
+        self.df = df
+
+        result = []
+
+        # 全パラメータをグリッドサーチ(計算量: Ο[n^3])
+        for i, span in enumerate(list(range(self._span_low, self._span_high, 1))):
+            for buy in list(range(self._buy_thres_low, self._buy_thres_high, 1)):
+                for sell in list(range(self._sell_thres_low, self._sell_thres_high, 1)):
+
+                    sys.stdout.write(f"\r[{i}/{len(list(range(self._span_low, self._span_high, 1)))-1}] 計算中...")
+                    profit = self._calculate_rsi_profit(df, span, buy, sell)
+
+                    result.append([span, buy, sell, profit])
+
+                    if profit > self._best_parms["profit"]:
+                        self._best_parms["span"] = span
+                        self._best_parms["buy_thres"] = buy
+                        self._best_parms["sell_thres"] = sell
+                        self._best_parms["profit"] = round(profit, 2)
+                        self.df["buy_thres"] = buy
+                        self.df["sell_thres"] = sell
+
+        print("\n計算終了")
+
+        return pd.DataFrame(result, columns=["span", "buy", "sell", "profit"])
+
+    def result_params(self):
+        """
+        Note: 解析結果パラメタを表示
+        """
+        return self._best_parms
+
+    def result_graph(self):
+        """
+        Note: matplotlibで解析結果グラフを表示
+        """
+
+        fig = plt.figure(figsize=(20, 8))
+
+        plt.subplot(211)
+        plt.plot(self.df["Close"], marker="o")
+        plt.ylabel("Stock Price[Yen]")
+        plt.grid()
+
+        plt.subplot(212)
+        plt.plot(ta.RSI(self.df["Close"], self._best_parms["span"]), marker="o")
+        plt.plot(self.df["buy_thres"], linestyle="dashed", color="red")
+        plt.plot(self.df["sell_thres"], linestyle="dashed", color="blue")
+        plt.ylabel("RSI[%]")
+        plt.legend(["RSI", "Buy signal", "Sell signal"])
+        plt.grid()
+
+        plt.tight_layout()
+        fig.show()
---
+++ optimizer_module/optimizer_slow_stoch.py
@@ -0,0 +1,140 @@
+# import libraries
+import sys
+from typing import List, Dict
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import talib as ta
+
+
+class SlowStochOptimizer(object):
+    """
+    Note:
+    スローストキャスティクス売買を仮想で行い、パラメータをグリッドサーチで最適化
+    パラメータ
+        :fastD  RSIのspan
+        :slowD  購入する閾値
+    """
+    def __init__(self):
+
+        self.df = None
+
+        self._fastk_low = None
+        self._fastk_high = None
+        self._slowk_low = None
+        self._slowk_high = None
+        self._slowd_low = None
+        self._slowd_high = None
+
+        self._best_parms = {
+            "fastk": 0,
+            "slowk": 0,
+            "slowd": 0,
+            "profit": 0
+        }
+
+    def _calculate_slow_stoch_profit(self, df, fastk_period: int, slowk_period: int, slowd_period: int) -> float:
+        stoch = ta.STOCH(df["High"], df["Low"], df["Close"],
+                         fastk_period=14,
+                         slowk_period=3,
+                         slowd_period=3)
+
+        df = pd.DataFrame([df["Close"].to_list(), stoch[0].to_list(), stoch[1].to_list()],
+                          columns=stoch[0].index,
+                          index=["Close", "slowK", "slowD"]).T
+
+        # Args
+        stock = 0
+        start_asset = 10000
+        asset = start_asset
+
+        for i in range(len(df)):
+            # buy
+            if df["slowD"][i] > df["slowK"][i] and df["slowD"][i - 1] < df["slowK"][i - 1]:
+                if asset != 0:
+                    stock = asset / df["Close"][i]
+                    asset = 0
+                # print(f"buy ({df.index[i]}) : {round(df['slowK'][i-1], 1)}/{round(df['slowD'][i-1], 1)} -> {round(df['slowK'][i], 1)}/{round(df['slowD'][i], 1)} | stock: {round(stock, 2)}")
+            # sell
+            if df["slowD"][i] < df["slowK"][i] and df["slowD"][i - 1] > df["slowK"][i - 1]:
+                if stock != 0:
+                    asset = stock * df["Close"][i]
+                    stock = 0
+                # print(f"sell({df.index[i]}) : {round(df['slowK'][i-1], 1)}/{round(df['slowD'][i-1], 1)} -> {round(df['slowK'][i], 1)}/{round(df['slowD'][i], 1)} | asset: {round(asset, 2)}")
+
+        # If you still have stock, sell it
+        last_asset = asset + stock * df["Close"][-1]
+        pl_amount_retio = last_asset / start_asset * 100
+        # print(f"PL Amount: {round(pl_amount_retio, 2)}")
+
+        return pl_amount_retio
+
+    def set_params(self,
+                   fastk: List[int],
+                   slowk: List[int],
+                   slowd: List[int]):
+        """
+        set param
+        Args:
+            fastk(List) : fast K range[low, high]
+            slowk(List) : slow K range[low, high]
+            slowd(List) : slow D range[low, high]
+        """
+        self._fastk_low = fastk[0]
+        self._fastk_high = fastk[1]
+        self._slowk_low = slowk[0]
+        self._slowk_high = slowk[1]
+        self._slowd_low = slowd[0]
+        self._slowd_high = slowd[1]
+
+    def run(self, df):
+
+        self.df = df
+
+        result = []
+
+        # 全パラメータをグリッドサーチ(計算量: Ο[n^3])
+        for i, fastk in enumerate(list(range(self._fastk_low, self._fastk_high, 1))):
+            for slowk in list(range(self._slowk_low, self._slowk_high, 1)):
+                for slowd in list(range(self._slowd_low, self._slowd_high, 1)):
+
+                    sys.stdout.write(f"\r[{i}/{len(list(range(self._fastk_low, self._fastk_high, 1)))-1}] 計算中...")
+                    profit = self._calculate_slow_stoch_profit(df, fastk, slowk, slowd)
+
+                    result.append([fastk, slowk, slowd, profit])
+
+                    if profit > self._best_parms["profit"]:
+                        self._best_parms["fastk"] = fastk
+                        self._best_parms["slowk"] = slowk
+                        self._best_parms["slowd"] = slowd
+                        self._best_parms["profit"] = round(profit, 2)
+
+        print("\n計算終了")
+
+        return pd.DataFrame(result, columns=["fastk", "slowk", "slowd", "profit"])
+
+    def result_graph(self):
+        stoch = ta.STOCH(self.df["High"], self.df["Low"], self.df["Close"],
+                         fastk_period=self._best_parms["fastk"],
+                         slowk_period=self._best_parms["slowk"],
+                         slowd_period=self._best_parms["slowd"])
+
+        fig = plt.figure(figsize=(20, 8))
+
+        plt.subplot(211)
+        plt.plot(self.df["Close"][(self._best_parms["slowk"] + self._best_parms["slowk"]):], marker="o")
+        plt.grid()
+        plt.ylabel("Stock Price[Yen]")
+
+        plt.subplot(212)
+        plt.plot(stoch[0], marker="o")
+        plt.plot(stoch[1], marker="o")
+        plt.legend(["Slow-K", "Slow-D"])
+        plt.ylabel("Slow-K/D[%]")
+        plt.grid()
+
+        plt.tight_layout()
+        fig.show()
+
+    def result_params(self):
+        return self._best_parms
